e0917469b8ade51319992a6681e654ded2cb4dec
vaha95 <vahtang.shengeliya@resolventagroup.com>
2022-05-30 15:38:48 +0300

    #6: Реализовать домен booking.

--- src/Repository/BookingRepository.php
+++ src/Repository/BookingRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\Booking;
+use App\Domain\Booking\Entity\Booking;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
