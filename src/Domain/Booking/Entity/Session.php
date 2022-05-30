0c6e946419cf38a14a563529fe028800cb1ce163
vaha95 <vahtang.shengeliya@resolventagroup.com>
2022-05-30 16:25:50 +0300

    #3: Реализовать модуль Session.

---
+++ src/Domain/Booking/Entity/Session.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Domain\Booking\Entity;
+
+use App\Repository\SessionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SessionRepository::class)]
+class Session
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\OneToMany(mappedBy: 'session', targetEntity: Booking::class, orphanRemoval: true)]
+    private $bookings;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $startAt;
+
+    public function __construct()
+    {
+        $this->bookings = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Booking>
+     */
+    public function getBookings(): Collection
+    {
+        return $this->bookings;
+    }
+
+    public function addBooking(Booking $booking): self
+    {
+        if (!$this->bookings->contains($booking)) {
+            $this->bookings[] = $booking;
+            $booking->setSession($this);
+        }
+
+        return $this;
+    }
+
+    public function getStartAt(): ?\DateTimeImmutable
+    {
+        return $this->startAt;
+    }
+
+    public function setStartAt(\DateTimeImmutable $startAt): self
+    {
+        $this->startAt = $startAt;
+
+        return $this;
+    }
+}
