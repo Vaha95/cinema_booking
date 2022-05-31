d6b30cd8fa896775935d0fe642c3f63d771d42b9
vaha95 <vahtang.shengeliya@resolventagroup.com>
2022-05-31 07:56:56 +0300

    #1: Реализовать модуль Film.

---
+++ src/Repository/FilmRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Domain\Booking\Entity\Film;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Film>
+ *
+ * @method Film|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Film|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Film[]    findAll()
+ * @method Film[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class FilmRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Film::class);
+    }
+}
