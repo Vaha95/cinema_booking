02d954575763187b6d85e7ad4c7caf34590eb1f0
Vaha95 <66629914+Vaha95@users.noreply.github.com>
2022-06-08 08:14:19 +0300

    #14: Реализовать отображение сеансов в браузере. (#22)
    * #6: Реализовать домен booking.
    
    * #3: Реализовать модуль Session.
    
    * #14: Реализовать отображение сеансов в браузере.
    
    Co-authored-by: vaha95 <vahtang.shengeliya@resolventagroup.com>

--- src/Domain/Booking/Repository/BookingRepository.php
+++ src/Domain/Booking/Repository/BookingRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Repository;
+namespace App\Domain\Booking\Repository;
 
 use App\Entity\Booking;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
---
+++ src/Domain/Booking/View/SessionView.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Domain\Booking\View;
+
+use App\Domain\Booking\Exception\NotPositiveRealNumberException;
+use App\Domain\Booking\Validator\PositiveRealNumberValidator;
+
+class SessionView
+{
+    public readonly string $startAt;
+    public readonly string $endAt;
+    public readonly string $date;
+    public readonly string $duration;
+
+    const HOURS_AND_MINUTES_DATETIME_FORMAT = 'H:i';
+    const RUS_MONTHS_DICTIONARY = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
+    /**
+     * @param \DateTimeImmutable $startAt
+     * @param string $name
+     * @param int $duration
+     * @param int $freePlaces
+     *
+     * @throws \Exception
+     * @throws NotPositiveRealNumberException
+     */
+    public function __construct(public readonly string $name, public readonly int $freePlaces, \DateTimeImmutable $startAt, int $duration)
+    {
+        PositiveRealNumberValidator::validate($duration);
+
+        $preTransformEndAt = new \DateTime($startAt->format(\DateTimeInterface::ATOM));
+
+        $this->startAt = $startAt->format('H:i');
+        $this->endAt = $preTransformEndAt
+            ->modify(sprintf('+ %d minutes', $duration))
+            ->format(self::HOURS_AND_MINUTES_DATETIME_FORMAT);
+        $this->date = $this->dateToStringFormatter($startAt);
+        $this->duration = $this->getDuration($duration);
+    }
+
+    private function getDuration(int $duration): string
+    {
+        $hours = floor($duration / 60);
+        $minutes = $duration % 60;
+
+        return $this->timeToStringFormatter($hours, $minutes);
+    }
+
+    private function dateToStringFormatter(\DateTimeImmutable $dateTime): string
+    {
+        return sprintf('%d %s',
+            $dateTime->format('d'),
+            self::RUS_MONTHS_DICTIONARY[$dateTime->format('m') - 1]
+        );
+    }
+
+    private function timeToStringFormatter(int $hours, int $minutes): string
+    {
+        return sprintf('%d ч %d м', $hours, $minutes);
+    }
+}
